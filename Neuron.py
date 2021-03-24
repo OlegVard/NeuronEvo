d4f593c2a45c8ec60cb3b600d4c84ea166ee1fa9
OlegVard <61628812+OlegVard@users.noreply.github.com>
2021-03-24 14:53:23 +0700

    add matrix

--- Neuron.py
+++ Neuron.py
@@ -1,2 +1,34 @@
 # file for network
+import random
 
+
+class Neuron:   # запись структуры сети в виде матрицы
+    matrix = []
+    size = 5
+
+    def init_matrix(self):
+        i = 0
+        j = 0
+        while i < self.size:
+            small_matr = []
+            while j < self.size:
+                if i == j:
+                    small_matr.append(0)
+                    j += 1
+                    continue
+                if random.random() >= 0.3:
+                    small_matr.append(random.randint(0, 1))
+                    j += 1
+                else:
+                    small_matr.append(0)
+                    j += 1
+            self.matrix.append(small_matr)
+            i += 1
+            j = 0
+        return self.matrix
+
+
+m = Neuron()
+ma = m.init_matrix()
+for i in range(5):
+    print(ma[i])
